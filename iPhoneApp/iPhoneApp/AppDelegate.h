261bb4cba172374c0a00d676646cfd35fd310529
Ivan Plantevin <ivan.p92@gmail.com>
2013-02-19 21:11:34 +0100

    My first iPhone App :-P

---
+++ iPhoneApp/iPhoneApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iPhoneApp
+//
+//  Created by Ivan Plantevin on 19-02-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ iPhoneApp/iPhoneApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iPhoneApp
+//
+//  Created by Ivan Plantevin on 19-02-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
