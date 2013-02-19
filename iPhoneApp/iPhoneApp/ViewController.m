fe954fca9313142065118b779099a247066c6a28
Ivan Plantevin <ivan.p92@gmail.com>
2013-02-19 21:15:03 +0100

    Small change
    Just to test git

--- iPhoneApp/iPhoneApp/ViewController.m
+++ iPhoneApp/iPhoneApp/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"The view was loaded succesfully");
 }
 
 - (void)didReceiveMemoryWarning
